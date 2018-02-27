65d90576e053c959c60385dc4974251583073cda
Christopher Moser <moser121@icloud.com>
2018-02-27 23:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ function nowServing(person){
 function currentLine(katzDeli){
  line =[]
  var i = 0
- while(line.length > i){
-   i++
- }
+ for()
 if (line.length === 0) {
   return 'The line is currently empty.'
   
